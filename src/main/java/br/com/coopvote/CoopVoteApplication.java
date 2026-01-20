a853483c4cb8806b6eda41614bccb17fb1e0292a
Saulo Dias Diniz <sauloddiniz@gmail.com>
2026-01-19 21:37:03 -0300

    configuracao inicial

---
+++ src/main/java/br/com/coopvote/CoopVoteApplication.java
@@ -0,0 +1,13 @@
+package br.com.coopvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopVoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoopVoteApplication.class, args);
+    }
+
+}
