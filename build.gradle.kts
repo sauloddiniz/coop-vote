74c0ba46cb082a01a8d9462f18acfa4882c2e234
Saulo Dias Diniz <sauloddiniz@gmail.com>
2026-01-19 21:37:22 -0300

    adiciona dependências e configurações para integração com banco de dados Oracle e Spring Cloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.5.9"
     id("io.spring.dependency-management") version "1.1.7"
 }
+val springCloudVersion by extra("2025.0.1")
 
 group = "br.com"
 version = "0.0.1-SNAPSHOT"
@@ -20,8 +21,21 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.projectlombok:lombok")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    runtimeOnly("com.oracle.database.jdbc:ojdbc11")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
 
 tasks.withType<Test> {
